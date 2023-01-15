430ca7aaa6dd7b72b49d0be9ec0b1d01e65754ff
Ashok Saravanan <ashko.saravanan@gmail.com>
2023-01-15 08:39:51 -0600

    Initial Commit

---
+++ GM South/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  GM South
+//
+//  Created by Ashok Saravanan on 1/15/23.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.red
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: username)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    // Need to set up view for when user logs in correct
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                      EmptyView()
+                    }
+                                   
+                    
+                }
+            }
+        }
+        .navigationBarHidden(true)
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "mario2023" {
+            wrongUsername = 0
+            if password.lowercased() == "abc123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GM South/GM_SouthApp.swift
@@ -0,0 +1,17 @@
+//
+//  GM_SouthApp.swift
+//  GM South
+//
+//  Created by Ashok Saravanan on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GM_SouthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
